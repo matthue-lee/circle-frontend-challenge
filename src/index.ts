dd72c6906b1c71c01ebf35f74da0018f30f72425
Matthew Lee <tansenmatt@gmail.com>
2025-01-08 11:45:32 +1300

    added frontend url

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,9 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'], // Replace with your actual frontend URL
+}));
 
 app.use('/books', booksRouter);
 
